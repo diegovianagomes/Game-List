7cd3e728e112ce3a3f0bb0e26cdc7afeb9f265dd
dvgba <diegovianagomes01@gmail.com>
2025-01-23 09:35:51 -0300

    Inicio do projeto

---
+++ src/test/java/com/diegoviana/gamelist/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoviana.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
